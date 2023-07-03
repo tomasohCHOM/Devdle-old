0dd828235183509f91a17431d0e382a55ea01892
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-07-03 11:46:54 -0700

    Created words.json and get the word onMount

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-  import Board from "./lib/Board.svelte";
+  import { onMount } from "svelte";
+  import words from "./data/words.json";
+  import Board from "./components/Board.svelte";
+
+  const WORDS_API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
+  let secretWord: string;
+  let guesses: string[] = Array(6).fill(null);
+
+  onMount(async () => {
+    secretWord = words[Math.floor(Math.random() * words.length)];
+  });
 </script>
 
 <main>
   <h1>WordleSvelte Clone</h1>
   <Board />
+  <div>{secretWord}</div>
 </main>
