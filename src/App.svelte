81e83aeeb7888eb98e111b0b144407a61eb09a7e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-07-03 12:35:43 -0700

    Set the board for Wordle

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,20 @@
   import { onMount } from "svelte";
   import words from "./data/words.json";
   import Board from "./components/Board.svelte";
+  import KeyBoard from "./components/KeyBoard.svelte";
 
-  const WORDS_API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
-  let secretWord: string;
+  let secretWord: string = "";
   let guesses: string[] = Array(6).fill(null);
+  let currentGuess: string = "";
 
   onMount(async () => {
     secretWord = words[Math.floor(Math.random() * words.length)];
+    const handleKeyType = (event: KeyboardEvent) => {
+      if (event.key >= "a" && event.key <= "z") currentGuess += event.key;
+      if (event.key === "Backspace")
+        currentGuess = currentGuess.slice(0, currentGuess.length - 1);
+    };
+    window.addEventListener("keydown", handleKeyType);
   });
 </script>
 
@@ -16,4 +23,6 @@
   <h1>WordleSvelte Clone</h1>
   <Board />
   <div>{secretWord}</div>
+  <div>{currentGuess}</div>
+  <KeyBoard />
 </main>
---
+++ src/components/KeyBoard.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  const keys = ["qwertyuiop", "asdfghjkl", "<zxcvbnm>"];
+  const clickKey = (key: string) => {
+    if (key == "<") key = "Backspace";
+    if (key == ">") key = "Enter";
+  };
+
+  let keycolors = {};
+  function updateKeyboard(colors) {
+    for (let i of Object.keys(colors)) {
+      for (let j of colors[i]) {
+        keycolors[j] = i;
+      }
+    }
+    keycolors = keycolors;
+  }
+</script>
+
+<main>
+  <br />
+  {#each keys as key}
+    <div class="row">
+      {#each key as letter}
+        <button
+          class="keyboard-key ${keycolors[letter]}"
+          on:click={() => {
+            clickKey(letter);
+          }}
+          >{#if letter === "<"}
+            DEL
+          {:else if letter === ">"}
+            ENTER
+          {:else}
+            {letter.toUpperCase()}
+          {/if}
+        </button>
+      {/each}
+    </div>
+  {/each}
+</main>
+
+<style>
+  .keyboard-key {
+    background-color: #e9e9e9;
+    margin: 0.125rem;
+  }
+</style>
