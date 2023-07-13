0b4d981b25d6c6bfabb138701c2d3ad68e019e96
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-07-13 12:36:06 -0700

    Commit to deploy #2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Devdle",
+  base: "/Devdle/",
   plugins: [svelte()],
 });
